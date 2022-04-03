565efc8f2893ae4c38b684a9030151240ece5426
Ivan Toselli <ivan.toselli31@gmail.com>
2022-04-03 14:53:51 +0200

    Stp we2: Exe 4 complete

--- Exe3/assets/js/main.js
+++ Exe3/assets/js/main.js
@@ -1,7 +1,7 @@
 /* Variabili */
 const myArray = ['Michele', 'Fabio', 'Roberto', 'Giovanni', 'Simone', 'Chiara'];
-const min = parseInt(prompt('Per il forEach: Inserisci per il delta min un numero tra 0 e 5'));
-const max = parseInt(prompt('Per il forEach: Inserisci per il delta max un numero tra 0 e 5'));
+const min = parseInt(prompt('Per il forEach: Inserisci per il Δ-min un numero tra 0 e 5'));
+const max = parseInt(prompt('Per il forEach: Inserisci per il Δ-max un numero tra 0 e 5'));
 let deltaArray = [];
 
 //Metodo con il ciclo forEach
@@ -15,8 +15,8 @@ myArray.forEach(
 console.log(deltaArray);
 
 // Metodo con il ciclo Filter
-const min1 = parseInt(prompt('Per il Filter: Inserisci per il delta min un numero tra 0 e 5'));
-const max1 = parseInt(prompt('Per il Filter: Inserisci per il delta max un numero tra 0 e 5'));
+const min1 = parseInt(prompt('Per il Filter: Inserisci per il Δ-min un numero tra 0 e 5'));
+const max1 = parseInt(prompt('Per il Filter: Inserisci per il Δ-max un numero tra 0 e 5'));
 
 const newdeltaArray = myArray.filter(
     (element, index) => min1 <= index && max1 >= index 
--- Exe4/assets/js/main.js
+++ Exe4/assets/js/main.js
@@ -1,75 +1,45 @@
 /* Variabili */
-let showBikesContainer = document.getElementById("showBikesContainer");
-let pes1 = 8.14;
-let pes2 = 9.11;
-let pes3 = 8.77;
-let pes4 = 8.34;
-let pes5 = 8.95;
-let pes6 = 8.52;
-let pes7 = 9.36;
-let pes8 = 8.10;
-let pes9 = 7.89;
+const targhe = [];
+let students = [
+    { name: 'Marco', id: 213, grades: 78 },
+    { name: 'Paola', id: 110, grades: 96 },
+    { name: 'Andrea', id: 250, grades: 48 },
+    { name: 'Gaia', id: 145, grades: 74 },
+    { name: 'Luigi', id: 196, grades: 68 },
+    { name: 'Piero', id: 102, grades: 50 },
+    { name: 'Francesca', id: 120, grades: 84 },
+  ];
 
+// Usando forEach
+students.forEach(
+    (el) =>{
+      targhe.push(el.name.toUpperCase());
+    }
+);
+console.log(targhe);
 
-/* Oggetti */
-// Ogni oggetto descrive una bici da corsa 
-// con proprietà: Nome e Peso.
-let dataBikes = [
-    {
-        'Nome': 'Bianchi Oltre XR4 Disc',
-        'Peso': pes1, 
-    }, 
-    {
-        'Nome': 'BMC Timemachine Road 01',
-        'Peso': pes2,
-    },
-    {
-        'Nome': 'Cannondale SystemSix Hi-MOD',
-        'Peso': pes3,
-    }, 
-    {
-        'Nome': 'BH G7 Disc',
-        'Peso': pes4,
-    },
-    {
-        'Nome': 'Argon 18 Nitrogen Disc',
-        'Peso': pes5,
-    }, 
-    {
-        'Nome': ' Canyon Endurace CF SLX Disc',
-        'Peso': pes6,
-    }, 
-    {
-        'Nome': 'Cinelli Superstar Disc',
-        'Peso': pes7, 
-    },
-    {
-        'Nome': 'Ridley Fenix SL Disc',
-        'Peso': pes8,
-    },  
-    {
-        'Nome': 'Trek Domane SLR 7 Disc',
-        'Peso': pes9,
-    } 
-];
+  // Usando map
+const newTarghe = students.map(
+    (el) => el.name.toUpperCase()
+);
+console.log(newTarghe);
 
-
-
-/* Funzioni */
-function showBikes() {
-    // Destrutturazione
-for (i = 0; i < 8; i++){
-    let {Nome: a, Peso: b} = dataBikes[i];
-    if (b > 8){
-        document.getElementById("showBikesContainer").innerHTML =
-        `<div class="d-flex row-12 m-5 bg-primary">
-            <h2 class="text-warning text-center align-self-center">Bicicletta con peso minore <h2>
-          <h3 class="text-center text-dark border m-3 p-2 bg-light">Nome: ${(a)}</h3>
-          <h3 class="text-center text-white border m-3 p-2 bg-secondary">Peso: ${(b)}</h3>
-        </div>`
+// Array di studenti con voti superiori a 70
+const deserving = students.filter(
+    (el) => {
+        if (el.grades > 70){
+            return true
         }
     }
-}
-/* Eventi */
-showBikes()
-console.log(dataBikes);
+);
+console.log(deserving); 
+
+// Array di studenti con voti superiori a 70 e ID superiore 120
+const honored = students.filter(
+    (el) => {
+        if (el.grades > 70 && el.id > 120){
+            return true
+        }
+    }
+);
+console.log(honored); 
